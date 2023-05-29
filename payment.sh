bea2a9df292e5eaaddb343a6641adffba05c372c
Rohith M <rohith.m068@gmail.com>
2023-05-30 00:54:23 +0530

    Creating Payment service sh files

--- payment.sh
+++ payment.sh
@@ -1,25 +1,25 @@
 echo -e '\e[33mInstall Python package\e[0m'
-yum install python36 gcc python3-devel -y
+yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
 
 echo -e '\e[33mUser Add Roboshop\e[0m'
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e '\e[33mMaking Directory App\e[0m'
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e '\e[33m Downloading the payment content\e[0m'
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
 cd /app
-unzip /tmp/payment.zip
+unzip /tmp/payment.zip &>>/tmp/roboshop.log
 
 echo -e '\e[33mPip installing requirements\e[0m'
 cd /app
-pip3.6 install -r requirements.txt
+pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
 
 echo -e '\e[33mSetting up Service\e[0m'
-cp /root/roboshop-shell/payment.service   /etc/systemd/system/payment.service
+cp /root/roboshop-shell/payment.service   /etc/systemd/system/payment.service &>>/tmp/roboshop.log
 
 echo -e '\e[33mStarting Service\e[0m'
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable payment &>>/tmp/roboshop.log
+systemctl restart payment &>>/tmp/roboshop.log
