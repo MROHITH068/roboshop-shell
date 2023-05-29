949bcdbab34985a9ecbbf67dbd2e3c19892761fa
Rohith M <rohith.m068@gmail.com>
2023-05-29 16:44:56 +0530

    Mongodb Service File

---
+++ catalogue.sh
@@ -0,0 +1,21 @@
+echo -e '\e[33mDownloading NodeJS repos\e[0m'
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e '\e[33mInstalling NodeJS\e[0m'
+yum install nodejs -y
+
+echo -e '\e[33mAdding user roboshop\e[0m'
+useradd roboshop
+
+echo -e '\e[33mMakinging directory roboshop\e[0m'
+rm -rf app
+mkdir /app
+
+echo -e '\e[33mDownloading catalogue Content\e[0m'
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+
+echo -e '\e[33mInstalling Dependencies\e[0m'
+npm install
