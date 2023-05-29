52d16810a3ce2b42d71c18ebd88648910fe62ed6
Rohith M <rohith.m068@gmail.com>
2023-05-29 18:25:57 +0530

    Updating MySQL service sh files

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,15 @@
 echo -e '\e[33mDisabling the required Module\e[0m'
-yum module disable mysql -y
+yum module disable mysql -y &>>/tmp/roboshop.log
 
 echo -e '\e[33mAdding the Repo File\e[0m'
-cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
 echo -e '\e[33mInstalling MySQL Server\e[0m'
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/roboshop.log
 
 echo -e '\e[33mStarting MySQL Service\e[0m'
-systemctl enable mysqld
-systemctl restart start mysqld
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl restart start mysqld &>>/tmp/roboshop.log
 
 echo -e '\e[33m Setup MySQL Password\e[0m'
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
