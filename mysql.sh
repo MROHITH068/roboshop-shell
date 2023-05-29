69c2e2102ef569ede33d56d807fe1906d05e9bcb
Rohith M <rohith.m068@gmail.com>
2023-05-29 18:49:49 +0530

    Updating mysql service sh files

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,7 @@ yum install mysql-community-server -y &>>/tmp/roboshop.log
 
 echo -e '\e[33mStarting MySQL Service\e[0m'
 systemctl enable mysqld &>>/tmp/roboshop.log
-systemctl restart start mysqld &>>/tmp/roboshop.log
+systemctl restart mysqld &>>/tmp/roboshop.log
 
 echo -e '\e[33m Setup MySQL Password\e[0m'
 mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
