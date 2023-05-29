c623d73d22ebb8f53123700cfe52c1e29600e4f5
Rohith M <rohith.m068@gmail.com>
2023-05-29 17:41:50 +0530

    Updating mongodb sh files

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,8 @@ cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/robos
 echo -e '\e[33mInstalling MongoDB Server\e[0m'
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
-#Modify the config file
+echo -e '\e[33mModify the config file\e[0m'
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 
 echo -e '\e[33mStart MongoDB Server\e[0m'
 systemctl enable mongod &>>/tmp/roboshop.log
