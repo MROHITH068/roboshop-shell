e7f0b7462fdfc66695974eecb5c26317188203eb
Rohith M <rohith.m068@gmail.com>
2023-05-29 12:53:10 +0530

    Mongodb Service File

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e '\e[33Copy MongoDB repo file\[e0m'
-cp mongodb.rep /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 echo -e '\e[33Installing MongoDB Server\[e0m'
 yum install mongodb-org -y
