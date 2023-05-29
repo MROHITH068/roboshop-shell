6f9fb3c2ecda60047cd12292b3b73b3aa45825b4
Rohith M <rohith.m068@gmail.com>
2023-05-29 17:08:41 +0530

    Updating frontend and Mongodb sh files

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,18 @@
+echo -e '\e[33mInstalling Nignx\e[0m'
 yum install nginx -y &>>/tmp/roboshop.log
 
+echo -e '\e[33mRemoving the old Content\e[0m'
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
+echo -e '\e[33mDownloading the frontend Content\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-cd /usr/share/nginx/html &>>/tmp/roboshop.log
-unzip /tmp/frontend.zip
+echo -e '\e[33mUnziping the frontend content\e[0m'
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
+echo -e '\e[33mRestarting the Nginx\e[0m'
 systemctl enable nginx
-systemctl restart nginx
+systemctl restart nginx &>>/tmp/roboshop.log
--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,11 @@
-echo -e '\e[33mCopy MongoDB repo file\[e0m'
+echo -e '\e[33mCopy MongoDB repo file\e[0m'
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
-echo -e '\e[33mInstalling MongoDB Server\[e0m'
+echo -e '\e[33mInstalling MongoDB Server\e[0m'
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 #Modify the config file
 
-echo -e '\e[33mStart MongoDB Server\[e0m'
+echo -e '\e[33mStart MongoDB Server\e[0m'
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
