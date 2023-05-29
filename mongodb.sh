a60cfda70f71a418f9651aeafb8e61f4ecaaeaca
Rohith M <rohith.m068@gmail.com>
2023-05-29 12:59:42 +0530

    Mongodb Service File

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,8 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e '\e[33mInstalling MongoDB Server\[e0m'
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
+#Modify the config file
+
 echo -e '\e[33mStart MongoDB Server\[e0m'
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
